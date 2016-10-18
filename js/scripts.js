ceaed9f64a504a6c42bf04cf9678a95259f088c2
Luke Keysboe and Steve Burton <steve@>
2016-10-18 15:59:12 -0700

    create flashcard website

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $(".card.clickable1").click(function() {
+    $(".header-showing1").toggle();
+    $(".header-hidden1").toggle();
+  });
+  $(".card.clickable2").click(function() {
+    $(".header-showing2").toggle();
+    $(".header-hidden2").toggle();
+  });
+  $(".card.clickable3").click(function() {
+    $(".header-showing3").toggle();
+    $(".header-hidden3").toggle();
+  });
+  $(".card.clickable4").click(function() {
+    $(".header-showing4").toggle();
+    $(".header-hidden4").toggle();
+  });
+  $(".card.clickable5").click(function() {
+    $(".header-showing5").toggle();
+    $(".header-hidden5").toggle();
+  });
+  $(".card.clickable6").click(function() {
+    $(".header-showing6").toggle();
+    $(".header-hidden6").toggle();
+  });
+});
